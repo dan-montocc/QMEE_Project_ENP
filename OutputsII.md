3b22e64e9cff6a517231cd427cdbc0e0631ee7d3
dan-montocc <montocds@mcmaster.ca>
2021-04-15 14:22:30 -0400

    DM take 2

---
+++ OutputsII.md
@@ -0,0 +1,29 @@
+__Linear regression model output summary tables__
+
+_Hypothesis 1_
+
+`Call:
+lm(formula = logBiomass ~ Depth + NH4 + ChlA + Sal_B + Temp_B + 
+    DO_B + Turb, data = all_fish_sub1)
+
+Residuals:
+     Min       1Q   Median       3Q      Max 
+-0.93566 -0.13590  0.00273  0.19784  0.92413 
+
+Coefficients:
+             Estimate Std. Error t value Pr(>|t|)  
+(Intercept) -1.052827   1.144031  -0.920   0.3602  
+Depth        0.624799   0.343769   1.817   0.0729 .
+NH4         -0.106421   0.042302  -2.516   0.0139 *
+ChlA        -0.059200   0.038080  -1.555   0.1240  
+Sal_B       -0.009440   0.007028  -1.343   0.1831  
+Temp_B      -0.009908   0.014331  -0.691   0.4914  
+DO_B         0.066180   0.042601   1.553   0.1243  
+Turb         0.013928   0.013579   1.026   0.3082  
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 0.3781 on 79 degrees of freedom
+Multiple R-squared:  0.3635,	Adjusted R-squared:  0.3071 
+F-statistic: 6.445 on 7 and 79 DF,  p-value: 5.19e-06`
+
---
+++ hypo_1_II.R
@@ -0,0 +1,73 @@
+##Hypothesis 1
+####Take 2....
+
+library(here)
+library(ggplot2)
+library(tidyverse)
+library(corrplot)
+
+
+#load in data
+all_fish <- read.csv(here("Joined_Cleaned_Data/Hypothesis_1/ALL_FishJoinWQ_1996to2005.csv"))
+all_fish <- all_fish[,-c(1)]
+fish_therm <- read.csv("Joined_Cleaned_Data/Hypothesis_1/FishThermGuild_Join_WQ_1996to2005.csv")
+fish_therm <- fish_therm[,-c(1)]
+spdat <- read.csv("Joined_Cleaned_Data/Hypothesis_1/FishSpecies_Join_WQ_1996to2005.csv")
+spdat <- spdat[,-c(1)]
+
+#check for variable redundancy in explanatory variables
+#response variables = TotalBiomass, SpeciesRichness
+#potential predictors = Temp (bottom and surface), DO (bottom and surface), Salinity (bottom and surface), water depth
+
+#evaluate collinearity of predictor variables with correlation matrix
+#code referenced from STHDA.com
+##http://www.sthda.com/english/wiki/correlation-matrix-an-r-function-to-do-all-you-need
+source("http://www.sthda.com/upload/rquery_cormat.r")
+all_fish_xsub <- all_fish[,c("Temp_S", "Temp_B","DO_S","DO_B","Depth","Sal_S","Sal_B", 
+                             "NH4", "ChlA","Turb")]
+rquery.cormat(all_fish_xsub)
+
+
+#build model all fish level
+all_fish$logBiomass <- log10(all_fish$TotalBiomass)
+fullfishmod <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B +
+                    Temp_B + DO_B + Turb, all_fish)
+plot(fullfishmod)
+
+#79 and 80 obs have high leverage
+##removed
+all_fish_sub1 <- all_fish[-c(79,80),]
+fullfishmod <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B +
+                    Temp_B + DO_B + Turb, all_fish_sub1)
+plot(fullfishmod)
+
+#look at categorical variable effect size on residuals
+boxplot(residuals(fullfishmod) ~ all_fish$Area) #area does not seem to effect the residuals disproportionately 
+boxplot(residuals(fullfishmod) ~ all_fish$Month)#March different
+summary(all_fish$Month == "March")# only one obs 
+summary(all_fish$Month == "May")
+##since study area is Florida, seasons are not as pronounced as they are in north
+boxplot(residuals(fullfishmod) ~ all_fish$Year) #no specific Year stands out
+
+##looks good
+summary(fullfishmod)
+
+#coefficient plot
+library(dotwhisker)
+library(broom)
+
+ov <- names(sort(coef(fullfishmod),decreasing=TRUE))
+
+dwplot(fullfishmod, order_vars = ovlist) %>%
+  relabel_predictors(c(Depth = "Depth",
+                       DO_B = "Dissolved oxygen",
+                       Turb = "Turbidity",
+                       Sal_B = "Salinity",
+                       Temp_B = "Temperature",
+                       ChlA = "Chlorophyll-a",
+                       NH4 = "NH4")) +
+  theme_bw() + xlab("Coefficient estimate") + ylab("") + theme(legend.position = "none") +
+  geom_vline(xintercept=0,lty=2)
+##only plotting two of 7??
+
+#build model without DO and temp to assess effect size of these two
