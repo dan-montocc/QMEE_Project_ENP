4cc812a21a26402ad5611070c58c56137e94462f
dan-montocc <montocds@mcmaster.ca>
2021-04-19 22:36:50 -0400

    DM hypo 1 commenting and clean

--- hypo_1_II.R
+++ hypo_1_II.R
@@ -12,8 +12,6 @@ all_fish <- read.csv(here("Joined_Cleaned_Data/Hypothesis_1/ALL_FishJoinWQ_1996t
 all_fish <- all_fish[,-c(1)]
 fish_therm <- read.csv("Joined_Cleaned_Data/Hypothesis_1/FishThermGuild_Join_WQ_1996to2005.csv")
 fish_therm <- fish_therm[,-c(1)]
-spdat <- read.csv("Joined_Cleaned_Data/Hypothesis_1/FishSpecies_Join_WQ_1996to2005.csv")
-spdat <- spdat[,-c(1)]
 
 #check for variable redundancy in explanatory variables
 #response variables = TotalBiomass, SpeciesRichness
@@ -28,7 +26,8 @@ all_fish_xsub <- all_fish[,c("Temp_S", "Temp_B","DO_S","DO_B","Depth","Sal_S","S
 rquery.cormat(all_fish_xsub)
 
 
-#ALL FISH MODEL 
+#ALL FISH MODEL
+##Full Model
 all_fish$logBiomass <- log10(all_fish$TotalBiomass)
 fullfishmod <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B +
                       Temp_B + DO_B + Turb, all_fish)
@@ -40,13 +39,13 @@ plot(fullfishmod)
 #79 and 80 obs have high leverage
 ##removed
 all_fish_sub1 <- all_fish[-c(79,80),]
-all_fish_scale <- all_fish_sub1 %>% mutate(across(where(is.numeric) & !logBiomass, ~drop(scale(.))))
+
 fullfishmod <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B +
                       Temp_B + DO_B + Turb, all_fish_sub1,
                   ## make sure NAs are included in residuals where appropriate
                   na.action=na.exclude)
 plot(fullfishmod)
-summary(fullfishmod)
+
 par(op) ## restore original graphical params
 
 #look at categorical variable effect size on residuals
@@ -60,28 +59,22 @@ boxplot(residuals(fullfishmod) ~ all_fish_sub1$Year) #no specific Year stands ou
 ##looks good
 summary(fullfishmod)
 
+#Scaled Full Model
+all_fish_scale <- all_fish_sub1 %>% mutate(across(where(is.numeric) & !logBiomass, ~drop(scale(.))))
+fullfishmod_scaled <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B +
+                           Temp_B + DO_B + Turb, all_fish_scale,
+                         ## make sure NAs are included in residuals where appropriate
+                         na.action=na.exclude)
+plot(fullfishmod_scaled)
+summary(fullfishmod_scaled)
 #coefficient plot
 library(dotwhisker)
 library(broom)
-fullfishmod_scaled <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B +
-                    Temp_B + DO_B + Turb, all_fish_scale,
-                  ## make sure NAs are included in residuals where appropriate
-                  na.action=na.exclude)
+
 ov <- names(sort(coef(fullfishmod_scaled),decreasing=TRUE))
 ov
 
-dwplot(fullfishmod_scaled, order_vars = ov) %>%
-  relabel_predictors(c(Depth = "Depth",
-                       DO_B = "Dissolved oxygen",
-                       Turb = "Turbidity",
-                       Sal_B = "Salinity",
-                       Temp_B = "Temperature",
-                       ChlA = "Chlorophyll-a",
-                       NH4 = "NH4")) +
-  theme_bw() + xlab("Coefficient estimate") + ylab("") + theme(legend.position = "none") +
-  geom_vline(xintercept=0,lty=2)
-##only plotting two of 7??
-
+#Scaled Full Model Coefficient plot
 dwplot(fullfishmod_scaled) %>%
   relabel_predictors(c(Depth = "Depth",
                        DO_B = "Dissolved oxygen",
@@ -93,13 +86,16 @@ dwplot(fullfishmod_scaled) %>%
   theme_bw() + xlab("Coefficient estimate") + ylab("") + theme(legend.position = "none") +
   geom_vline(xintercept=0,lty=2)
 
-
+#Subset Model
 #build model without DO and temp to assess effect size of these two
+#scaled
 subfishmod_scaled <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B + Turb, all_fish_scale,
-                                                                    na.action=na.exclude)
-subfishmod2 <- lm(logBiomass ~ Depth + DO_B + NH4 + ChlA + Sal_B + Turb, all_fish_sub1)
+                        na.action=na.exclude)
+#NOT scaled
+subfishmod <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B + Turb, all_fish_sub1,
+                 na.action=na.exclude)
 
-subfishmod_scaled <- lm(logBiomass ~ Depth + NH4 + ChlA + Sal_B + Turb, all_fish_scale)
+#Scaled Full and Subset Model Coefficient Plot
 dwplot(list(fullfishmod_scaled,subfishmod_scaled)) %>%
   relabel_predictors(c(Depth = "Depth",
                        DO_B = "Dissolved oxygen",
@@ -113,9 +109,11 @@ dwplot(list(fullfishmod_scaled,subfishmod_scaled)) %>%
   theme(legend.background = element_rect(colour="white"),
         legend.title = element_blank()) 
 
+#Scaled Models ANOVA
 anova(fullfishmod_scaled,subfishmod_scaled)
 
-dwplot(list(fullfishmod,subfishmod2)) %>%
+#NOT Scaled Subset and Full Model Coefficient plot
+dwplot(list(fullfishmod,subfishmod)) %>%
   relabel_predictors(c(Depth = "Depth",
                        DO_B = "Dissolved oxygen",
                        Turb = "Turbidity",
@@ -129,25 +127,10 @@ dwplot(list(fullfishmod,subfishmod2)) %>%
         legend.background = element_rect(colour="grey80"),
         legend.title = element_blank()) 
 
-anova(fullfishmod,subfishmod2)
+#NOT scaled Subset and Full Model ANOVA
+anova(fullfishmod,subfishmod)
 
-subfishmod3 <- lm(logBiomass ~ Depth + Temp_B + NH4 + ChlA + Sal_B + Turb, all_fish_sub1)
 
-dwplot(list(fullfishmod,subfishmod3)) %>%
-  relabel_predictors(c(Depth = "Depth",
-                       DO_B = "Dissolved oxygen",
-                       Turb = "Turbidity",
-                       Sal_B = "Salinity",
-                       Temp_B = "Temperature",
-                       ChlA = "Chlorophyll-a",
-                       NH4 = "NH4")) +
-  theme_bw() + xlab("Coefficient estimate") + ylab("") + 
-  geom_vline(xintercept=0,lty=2) + 
-  theme(legend.justification = c(0, 0),
-        legend.background = element_rect(colour="grey80"),
-        legend.title = element_blank()) 
-
-anova(fullfishmod,subfishmod3)
 
 #THERMAL GUILD MODEL
 
@@ -166,6 +149,7 @@ fish_therm_sub1$Year <- as.factor(fish_therm_sub1$Year)
 fish_therm_sub1$ThermalGuild <- as.factor(fish_therm_sub1$ThermalGuild)
 summary(fish_therm_sub1)
 
+#Full Model
 #build full model with thermal guild interaction
 fishthermmod <- lm (logBiomass ~ (Depth + NH4 + ChlA + Sal_B +
                       Temp_B + DO_B + Turb)*ThermalGuild, fish_therm_sub1)
@@ -179,71 +163,7 @@ plot(fishthermmod)
 summary(fishthermmod)
 
 #creating faceted coefficient plot for thermal guild data
-library(coefplot)
-
-modelCI <- coefplot:::buildModelCI(fishthermmod)
-ov2 <- names(sort(coef(fishthermmod),decreasing=TRUE))
-ov2
-coefname <- list("Warm guild","Depth:Cool/warm guild","Depth", "Depth:Warm guild",
-                 "Dissolved oxygen","Chlorophyll-a:Warm guild", "Turbidity:Cool/warm guild",
-                 "Turbidity:Warm guild", "Temperature", "Salinity:Cool/warm guild",
-                 "Turbidity", "Salinity", "Chlorophyll-a", "Salinity:Warm guild",
-                 "NH4", "Temperature:Cool/warm guild", "NH4:Warm guild", "Chlorophyll-a:Cool/warm guild",
-                 "NH4:Cool/warm guild", "Temperature:Warm guild", "Dissolved oxygen:Cool/warm guild",
-                 "Dissolved oxygen:Warm guild", "Cool/warm guild")
-
-coefplot:::buildPlotting.default(
-  modelCI, ylab = "", title = NULL, xlab = "Coefficient estimate", color = "indianred",
-  cex = 0.8, textAngle = 0, numberAngle = 0, shape = 16, linetype = 1, outerCI = 2,
-  innerCI = 1, multi = FALSE, zeroColor = "grey", zeroLWD = 1, zeroType = 2, numeric = FALSE,
-  fillColor = "grey", alpha = 1/2, horizontal = FALSE, facet = TRUE, scales = "free",
-  value = "Coefficient estimate", coefficient = "coefname", errorHeight = 0, dodgeHeight = 1,
-  interactive = TRUE)
-
-#attempt 2
-dwplot(fishthermmod) %>%
-  relabel_predictors(c(Depth = "Depth",
-                       DO_B = "Dissolved oxygen",
-                       Turb = "Turbidity",
-                       Sal_B = "Salinity",
-                       Temp_B = "Temperature",
-                       ChlA = "Chlorophyll-a",
-                       NH4 = "NH4")) +
-  theme_bw() + xlab("Coefficient estimate") + ylab("") + theme(legend.position = "none") +
-  geom_vline(xintercept=0,lty=2) + facet_wrap(~ThermalGuild)
-
-
-dwplot(fishthermmod) %>%
-  relabel_predictors(c("ThermalGuildwarm" = "Warm guild",
-                       "Depth:ThermalGuildcool/warm" = "Depth:Cool/warm guild",
-                       "Depth" = "Depth", 
-                       "Depth:ThermalGuildwarm" = "Depth:Warm guild",
-                       "DO_B" = "Dissolved oxygen",
-                       "ChlA:ThermalGuildwarm" = "Chlorophyll-a:Warm guild", 
-                       "Turb:ThermalGuildcool/warm" = "Turbidity:Cool/warm guild",
-                       "Turb:ThermalGuildwarm" = "Turbidity:Warm guild", 
-                       "Temp_B" = "Temperature", 
-                       "Sal_B:ThermalGuildcool/warm" = "Salinity:Cool/warm guild",
-                       "Turb" = "Turbidity", 
-                       "Sal_B" = "Salinity", 
-                       "ChlA" = "Chlorophyll-a", 
-                       "Sal_B:ThermalGuildwarm" = "Salinity:Warm guild",
-                       NH4 = "NH4", 
-                       "Temp_B:ThermalGuildcool/warm" = "Temperature:Cool/warm guild", 
-                       "NH4:ThernalGuildwarm" = "NH4:Warm guild", 
-                       "ChlA:ThermalGuildcool/warm" = "Chlorophyll-a:Cool/warm guild",
-                       "NH4:ThermalGuildcool/warm" = "NH4:Cool/warm guild", 
-                       "Temp_B:ThernalGuildwarm" = "Temperature:Warm guild", 
-                       "DO_B:ThermalGuildcool/warm" = "Dissolved oxygen:Cool/warm guild",
-                       "DO_B:ThermalGuildwarm" = "Dissolved oxygen:Warm guild", 
-                       "ThermalGuildcool/warm" = "Cool/warm guild")) +
-  theme_bw() + xlab("Coefficient estimate") + ylab("") + theme(legend.position = "none") +
-  geom_vline(xintercept=0,lty=2)
-
-## attempt 3 (BMB)
-
-## first tidy
-
+## BMB's code
 
 ## this automates most of the name changes
 rename_term1 <- function(x) {
@@ -288,15 +208,14 @@ tt1 <- (broom::tidy(fishthermmod_sep, conf.int=TRUE)
     %>% mutate(across(term, ~reorder(factor(.), estimate)))
 )
 
-gg0 <- (ggplot(tt, aes(estimate, term))
-    + geom_pointrange(aes(xmin=conf.low, xmax=conf.high))
-    + geom_vline(xintercept=0, lty=2)
-)
+#gg0 <- (ggplot(tt, aes(estimate, term))
+    #+ geom_pointrange(aes(xmin=conf.low, xmax=conf.high))
+    #+ geom_vline(xintercept=0, lty=2)
+#)
 
-print(gg0 + facet_wrap(~thermal_guild, labeller=label_both))
+#print(gg0 + facet_wrap(~thermal_guild, labeller=label_both))
 
-## or (this is more compact, and better if you want to compare guilds)
-
+#Guild Coefficient Plot
 library(colorspace)
 print(ggplot(tt, aes(estimate, term))
     + geom_pointrange(aes(xmin=conf.low, xmax=conf.high,
@@ -307,8 +226,11 @@ print(ggplot(tt, aes(estimate, term))
       theme_bw() + xlab("Coefficient estimate") + ylab("") + labs(color='Thermal Guild') 
     )
 
+
+
 ##SPECIES RICHNESS MODEL
-all_fish_scale <- all_fish %>% mutate(across(where(is.numeric) & !logBiomass, ~drop(scale(.))))
+
+#NOT Scaled Full Model
 SpRich_mod1 <- glm(SpeciesRichness ~ Depth + NH4 + ChlA + Sal_B +
                      DO_B + Temp_B + Turb, all_fish,family=poisson)
 plot(SpRich_mod1)
@@ -319,20 +241,35 @@ SpRich_mod1 <- glm(SpeciesRichness ~ Depth + NH4 + ChlA + Sal_B +
 plot(SpRich_mod1)
 summary(SpRich_mod1)
 
+#Scaled Full Model
+Rich_fish_scale <- all_fish_sub2 %>% mutate(across(where(is.numeric) & !SpeciesRichness, ~drop(scale(.))))
+
+SpRich_mod_scale <- glm(SpeciesRichness ~ Depth + NH4 + ChlA + Sal_B +
+                     DO_B + Temp_B + Turb, Rich_fish_scale,family=poisson)
+plot(SpRich_mod_scale)
+summary(SpRich_mod_scale)
+
+#NOT Scaled Subset Model
 SpRich_mod2 <- glm(SpeciesRichness ~ Depth + NH4 + ChlA + Sal_B +
                      Turb, all_fish_sub2,family=poisson)
 
-ov3 <- names(sort(coef(SpRich_mod1),decreasing=TRUE))
+#Scaled Subset Model
+SpRich_mod2_scale <- glm(SpeciesRichness ~ Depth + NH4 + ChlA + Sal_B +
+                     Turb, Rich_fish_scale,family=poisson)
+
+#NOT Scaled Full and Subset Model Coefficient plot
+library(dotwhisker)
+ov3 <- names(sort(abs(coef(SpRich_mod1)),decreasing=TRUE))
 ov3
 
 dwplot(list(SpRich_mod1,SpRich_mod2)) %>%
-  relabel_predictors(c(DO_B = "Dissolved oxygen",
+  relabel_predictors(c(Depth = "Depth",
+                       Sal_B = "Salinity",
+                       DO_B = "Dissolved oxygen",
                        Turb = "Turbidity",
                        Temp_B = "Temperature",
                        NH4 = "NH4",
-                       "ChlA" = "Chlorophyll-a",
-                       Sal_B = "Salinity",
-                       Depth = "Depth")) +
+                       ChlA = "Chlorophyll-a")) +
   theme_bw() + xlab("Coefficient estimate") + ylab("") + 
   geom_vline(xintercept=0,lty=2) + 
   theme(legend.background = element_rect(colour="white"),
@@ -349,3 +286,38 @@ dwplot(SpRich_mod1) %>%
   theme_bw() + xlab("Coefficient estimate") + ylab("") + 
   geom_vline(xintercept=0,lty=2) + 
   theme(legend.position = "none") 
+
+#Scaled Full and Subset Model Coefficient plot
+ov3 <- names(sort(abs(coef(SpRich_mod_scale)),decreasing=TRUE))
+ov3
+
+dwplot(list(SpRich_mod_scale,SpRich_mod2_scale)) %>%
+  relabel_predictors(c(Depth = "Depth",
+                       Sal_B = "Salinity",
+                       DO_B = "Dissolved oxygen",
+                       Turb = "Turbidity",
+                       Temp_B = "Temperature",
+                       NH4 = "NH4",
+                       ChlA = "Chlorophyll-a")) +
+  theme_bw() + xlab("Coefficient estimate") + ylab("") + 
+  geom_vline(xintercept=0,lty=2) + 
+  theme(legend.background = element_rect(colour="white"),
+        legend.title = element_blank()) 
+
+dwplot(SpRich_mod1) %>%
+  relabel_predictors(c(DO_B = "Dissolved oxygen",
+                       Turb = "Turbidity",
+                       Temp_B = "Temperature",
+                       NH4 = "NH4",
+                       "ChlA" = "Chlorophyll-a",
+                       Sal_B = "Salinity",
+                       Depth = "Depth")) +
+  theme_bw() + xlab("Coefficient estimate") + ylab("") + 
+  geom_vline(xintercept=0,lty=2) + 
+  theme(legend.position = "none") 
+
+#NOT Scaled Full and Sub ANOVA
+anova(SpRich_mod1,SpRich_mod2)
+
+#Scaled Full and Sub ANOVA
+anova(SpRich_mod_scale,SpRich_mod2_scale)
